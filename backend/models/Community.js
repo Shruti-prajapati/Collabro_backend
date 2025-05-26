234de8d6dd3691816285b79da702aee7dff5d139
Shruti Prajapati <101646320+Shruti-prajapati@users.noreply.github.com>
2025-05-26 12:10:25 +0530

    Update Community.js

--- backend/models/Community.js
+++ backend/models/Community.js
@@ -1,7 +1,17 @@
-const communitySchema = new mongoose.Schema({
+const communitySchema = new mongoose.Schema({ 
     name: String,
     description: String,
     members: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
     createdAt: { type: Date, default: Date.now },
   });
-  module.exports = mongoose.model("Community", communitySchema);
+  
+communitySchema.set('toJSON', {
+  virtuals: true,
+  versionKey: false,
+  transform: (_, ret) => {
+    ret.id = ret._id;
+    delete ret._id;
+  }
+});
+
+module.exports = mongoose.model("Community", communitySchema);
