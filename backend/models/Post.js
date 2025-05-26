74db57fb2deb5224d143273deb0561333a98eea7
Shruti Prajapati <101646320+Shruti-prajapati@users.noreply.github.com>
2025-05-26 13:39:26 +0530

    Update Post.js

--- backend/models/Post.js
+++ backend/models/Post.js
@@ -15,5 +15,14 @@ const postSchema = new mongoose.Schema({
     tags: [String],
     createdAt: { type: Date, default: Date.now },
   });
+
+postSchema.set("toJSON", {
+  virtuals: true,
+  versionKey: false,
+  transform: (_, ret) => {
+    ret.id = ret._id;
+    delete ret._id;
+  },
+});
   module.exports = mongoose.model("Post", postSchema);
   
